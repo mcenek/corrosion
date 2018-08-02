4dd62fb21c959961a44bcfb00c0ca6485798cf2e
dcrowder2 <dcrowder2@alaska.edu>
2018-08-02 13:17:51 -0800

    Using new texture calculations

--- cbcc/visualize_texture.py
+++ cbcc/visualize_texture.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-	if len(sys.argv) == 2:
+	if len(sys.argv) == 3:
 		print("Loading image " + sys.argv[1])
 		image = cv2.imread(sys.argv[1])
+		sd_matrix = np.load(sys.argv[2])
 		shape = image.shape
 		print(shape)
 		print("Extracting features")
-		features = feature.run_image(image)
+		features = feature.run_image(image, sd_matrix)
 		textures = features[:, :6]
 		print(textures[:, 0].shape)
-		clipped = np.clip(textures[:, 0], 0, 2000)
-		plt.boxplot(clipped, vert=False)
+		plt.boxplot(textures, vert=False)
 		plt.show()
 	else:
 		print("Please add a path to an image")
